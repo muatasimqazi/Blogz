dd38a939ebf10132f20ec47d5a46d6120f50b28a
Muatasim Qazi <muatasimqazi@gmail.com>
2017-07-20 22:43:19 -0700

    changed allowed routes

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def display_posts():
 
 @app.before_request
 def require_login():
-    allowed_routes = ['index', 'display_posts', 'login', 'signup']
+    allowed_routes = ['index', 'display_posts', 'login', 'signup', 'static']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
